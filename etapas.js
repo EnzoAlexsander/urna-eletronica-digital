64a00b52bca47e88d5ad5b4c7a8b79668c4fd070
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-01-20 19:54:43 -0300

    Etapas

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '10101',
+                nome: 'Daniel Alves',
+                partido: 'PGC',
+                fotos:[
+                    { url: 'danielalves.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '15158',
+                nome: 'Hernanes Profeta',
+                partido: 'PP',
+                fotos:[
+                    { url: 'hernanes.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '30',
+                nome: 'Brenner',
+                partido: 'PGOL',
+                vice: 'Luciano',
+                fotos: [
+                    { url: 'brenner.png', legenda: 'Prefeito' },
+                    { url: 'luciano.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'Reinaldo',
+                partido: 'PPC',
+                vice: 'Gabriel Sara',
+                fotos: [
+                    { url: 'reinaldo.png', legenda: 'Prefeito' },
+                    { url: 'gabrielsara.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
